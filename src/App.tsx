4aacb5246b0dfdd1314b1991664c12d7219bfdb0
Djibril <issoufoudjib@gmail.com>
2022-01-23 06:38:18 -0500

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" />
         <Route path="menu" element={<Menu />} />
         <Route path="code-verification" element={<CodeVerificationForm />} />
         <Route path="home" element={<Home />} />
         <Route path="game" element={<GamePage />} />
         <Route path="vote" element={<VotePage />} />
         <Route path="onboarding" element={<Onboarding />} />
-        <Route index element={<Menu />} />
+        <Route index element={<Home />} />
       </Routes>
     </div>
   );
